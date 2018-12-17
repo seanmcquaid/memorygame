25a944124e4ab0744b0925c3c6448e237f5b8ac2
seanmcquaid <sean.mcquaid2@verizon.net>
2018-12-17 15:13:28 -0500

    basic matching completed

--- scripts.js
+++ scripts.js
@@ -3,10 +3,10 @@
 $(document).ready(()=>{
     // will target the thing clicked on bc of this
     $("button").click(function(){
-        console.log($(this));
+        // console.log($(this));
         // attr method will get the value of that attribute
         let gridSize = $(this).attr("diff");
-        console.log(gridSize);
+        // console.log(gridSize);
         let cards = [];
         // our monsters start at 1 so start i at 1
         // every time through the loop, we push 2 monsters on because this is a matching game, we need 2 
@@ -33,10 +33,33 @@ $(document).ready(()=>{
             </div>
             `
         })
-        console.log(memoryHTML);
+        // console.log(memoryHTML);
         $(".memory-game").html(memoryHTML);
+
+        // user just clicked on a card
         $(".card-holder").click(function(){
             $(this).addClass("flip");
+            let cardsUp = $(".flip");
+            // if cards up has two elemetns, then this is the 2nd card
+            if (cardsUp.length === 2){
+                const card1 = cardsUp[0];
+                const card2 = cardsUp[1];
+                if(card1.innerHTML == card2.innerHTML){
+                    // these cards match
+                    // remove flip
+                    cardsUp.removeClass("flip");
+                    cardsUp.addClass("matched");
+                } else{
+                    // these are not a match bc the html is
+                    // js is too fast, we have to let the user see the card before we flip it back
+                    // settimeout to the resue
+                    setTimeout(()=>{
+                        cardsUp.removeClass("flip");
+                    }, 2000);
+                }
+            } else {
+                // only one card card up, do nothing
+            }
         })
     })
 })
